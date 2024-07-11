b9bda11e1e63223b586fafbcf242803c3d6bfe59
biooids <biooidsbiooid@gmail.com>
2024-07-12 00:21:59 +0200

    "Added navigation to AlbumItem component and Display component, and updated Navbar component to use useNavigate hook."

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DisplayHome from "./DisplayHome";
 import { Route, Routes } from "react-router-dom";
+import DisplayAlbum from "./DisplayAlbum";
 
 function Display() {
   return (
     <div className="w-[100%] m-2 px-6 pt-4 rounded bg-slate-900 text-white overflow-auto lg:w-[75%]  lg:ml-0">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
